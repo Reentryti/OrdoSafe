068e0cd6df02b6efc450ba0bd28e0a18959ccee0
Reentryti <reentryti5@outlook.fr>
2025-06-16 00:53:06 +0000

    Auth Endpoint
    and some changes

--- Backend/utilisateurs/urls.py
+++ Backend/utilisateurs/urls.py
@@ -2,8 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.custom_login, name='login'),
-    path('login/2fa/', views.login_2fa, name='login_2fa'),
     path('account/2fa/setup/', views.setup_2fa, name='setup_2fa'),
     path('account/2fa/backup-codes/', views.backup_codes, name='backup_codes'),
+
+    path('patient/login/', views.patient_login, name='patient_login'),
+    path('patient/login/2fa/', views.patient_login_2fa, name='patient_login_2fa'),
+    path('patient/signup/', views.patient_signup, name='patient_signup'),
+
+    path('doctor/login/', views.doctor_login, name='doctor_login'),
+    path('doctor/login/2fa', views.doctor_login_2fa, name='doctor_login_2fa'),
+    path('doctor/signup', views.doctor_signup, name='doctor_signup'),
+
+    path('pharmacist/login', views.pharmacist_login, name='pharmacist_login'),
+    path('pharmacist/login_2fa', views.pharmacist_login_2fa, name='pharmacist_login_2fa'),
+    path('pharmacist/signup', views.pharmacist_signup, name='pharmacist_signup'),
 ]
