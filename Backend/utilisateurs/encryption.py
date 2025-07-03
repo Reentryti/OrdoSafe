a7744341275f3a0efe9506787bb5de2f1b75fbbf
Reentryti <reentryti5@outlook.fr>
2025-07-03 13:03:57 +0000

    Binary Data encoding error

--- Backend/utilisateurs/encryption.py
+++ Backend/utilisateurs/encryption.py
@@ -15,12 +15,28 @@ class FernetEncryption:
     def encrypt(self, data):
         if data is None:
             return None
-        return self.cipher_suite.encrypt(data.encode()).decode()
+        if isinstance(data, str):
+            data_bytes = data.encode('utf-8')
+        elif isinstance(data, bytes):
+            data_bytes = data
+        else:
+            data_bytes = str(data).encode('utf-8')
+
+        encrypted_bytes = self.cipher_suite.encrypt(data_bytes)
+        return encrypted_bytes.encode('utf-8')
 
     # Decrypt data
     def decrypt(self, encrypted_data):
         if encrypted_data is None:
             return None
-        return self.cipher_suite.decrypt(encrypted_data.encode()).decode()
+        
+        if isinstance(encrypted_data, str):
+            encrypted_bytes = encrypted_data.encode('utf-8')
+        elif isinstance(encrypted_data, bytes):
+            encrypted_bytes = encrypted_data
+        else:
+            encrypted_bytes = str(encrypted_data).encode('utf-8')
+        decrypted_bytes = self.cipher_suite.decrypt(encrypted_bytes)
+        return decrypted_bytes
 
 fernet = FernetEncryption()
